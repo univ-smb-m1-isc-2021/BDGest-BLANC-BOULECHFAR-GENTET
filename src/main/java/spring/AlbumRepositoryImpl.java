9e6399180051d447dd72948b115c18e7665cecfc
juliette-blanc <juliette.blanc@etu.univ-savoie.fr>
2022-03-28 17:27:53 +0200

    Update AlbumRepositoryImpl.java

--- src/main/java/spring/AlbumRepositoryImpl.java
+++ src/main/java/spring/AlbumRepositoryImpl.java
@@ -7,19 +7,18 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 @RequiredArgsConstructor
+@SuppressWarnings("unchecked")
 public class AlbumRepositoryImpl implements AlbumRepository {
 
     final @NonNull EntityManager em;
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<Album> getListAlbum() {
         return em.createQuery("SELECT a FROM Album a")
                 .getResultList();
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void addAlbum(Album album) {
         em.persist(album);
     }
