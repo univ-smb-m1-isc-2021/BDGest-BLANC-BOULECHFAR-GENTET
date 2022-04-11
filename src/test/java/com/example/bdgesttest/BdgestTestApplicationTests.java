73167be153b54b58b0b33fd2a0adbca41d2ce02b
willygentet <58994972+willygentet@users.noreply.github.com>
2022-04-11 15:28:45 +0200

    marche ga stp

---
+++ src/test/java/com/example/bdgesttest/BdgestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bdgesttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdgestTestApplicationTests {
+
+    /*@Test
+    void contextLoads() {
+    }*/
+
+}
